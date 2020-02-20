1b76c59f539792dae7edcb068233274291b70d05
PresSiomai <paul.salcedo13@gmail.com>
2020-02-21 04:26:57 +0800

    SIOMAI

--- zerterra_cms/delivered.php
+++ zerterra_cms/delivered.php
@@ -59,7 +59,7 @@ include 'Buttons/approvedQuery.php';
           <a href="users.php" class="w3-bar-item w3-button" id="item-hover"><i class="fas fa-user"></i> &nbsp Users</a>
           <a class="w3-bar-item w3-button" id="sendmodal" ><i class="fas fa-cubes"></i> &nbsp Orders</a>
           <a href="request.php" class="w3-bar-item w3-button" id="item-hover"><i class="fas fa-envelope-open-text"></i> &nbsp Request</a>
-          <a href="sales.php" class="w3-bar-item w3-button" id="item-hover"><i class="fas fa-hand-holding-usd"></i> &nbsp sales</a>
+          <a href="sales.php" class="w3-bar-item w3-button" id="item-hover"><i class="fas fa-hand-holding-usd"></i> &nbsp Sales</a>
           <a href="#" class="w3-bar-item w3-button" id="item-hover"><i class="fas fa-print"></i> &nbsp Consolidate</a>
         </div>
 
@@ -130,7 +130,7 @@ include 'Buttons/approvedQuery.php';
                               include 'searchFunction/searchDeliveredFunction.php';
                             }
                           }else{     
-                            $sql = "SELECT * FROM approveorders_list WHERE is_delivered='1' ORDER BY id DESC LIMIT $offset, $no_of_records_per_page";
+                            $sql = "SELECT * FROM delivered_order_list WHERE is_activated='1' ORDER BY id DESC LIMIT $offset, $no_of_records_per_page";
                             $res_data = $con->query($sql);
                             while($row = mysqli_fetch_array($res_data)){ 
                               {

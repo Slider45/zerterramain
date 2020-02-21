462c93e2ce51d5f87ae160fd34c1e84a5ccd7187
pauladremesin <60042885+pauladremesin@users.noreply.github.com>
2020-02-22 00:47:41 +0800

    updated user

---
+++ zerterraph_user/change-email.php
@@ -0,0 +1,161 @@
+
+<?php
+
+session_start();
+
+
+include '../PagesFunction/connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:600&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/plainlogo.png" type="image/x-icon" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+    <script src="js/homeJS.js"></script>
+    <link rel="stylesheet" type="text/css" href="sass/change-password.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <title>zerTERRA</title>
+</head>
+
+<body>
+
+   <!--NAVBAR-->
+
+   
+<?php
+include 'user-header.php';
+
+?>
+
+    <!-- script navbar -->
+
+
+    <script>
+        (function () {
+            var burger = document.querySelector('.burger');
+            var nav = document.querySelector('#' + burger.dataset.target);
+            burger.addEventListener('click', function () {
+                burger.classList.toggle('is-active');
+                navMenu.classList.toggle('is-active');
+
+            });
+
+
+        })();
+    </script>
+
+    <!-- END OF NAVBAR -->
+
+    <!-- SIDE BAR -->
+    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left">
+    <h3 class="w3-bar-item">Name</h3> 
+    <hr class="hr"></hr>
+    <a href="index.php" class="w3-bar-item w3-button w3-hover-white">Dashboard</a>
+    <a href="profile.php" class="w3-bar-item w3-button w3-hover-white">Profile</a>
+
+    <button class="w3-button w3-block w3-left-align w3-hover-white" id="acc-setting" onclick="myAccFunc()">Account Settings &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fa fa-caret-down"></i></button>
+    <div id="demoAcc" class="w3-hide w3-white w3-card">
+        <a href="acc_setting.php" class="w3-bar-item w3-button w3-hover-white"><i class="fas fa-pen-nib"></i> &nbsp&nbspEdit Profile</a>
+        <a href="change-password.php" class="w3-bar-item w3-button w3-hover-white"><i class="fas fa-lock"></i> &nbsp&nbspChange Password</a>
+    </div>
+
+    <a href="request-repair.php" class="w3-bar-item w3-button w3-hover-white">Request for Repair</a>
+    <a href="contact-us.php" class="w3-bar-item w3-button w3-hover-white">Contact Us</a>
+    </div>
+
+    <script>
+    function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
+    }
+
+    function myAccFunc1() {
+    var x = document.getElementById("demoAcc1");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
+    }
+    </script>
+
+    <!-- END OF SIDE BAR -->
+    <section class="section">
+    <div class="container" id="form_edit">
+        <div class="header">
+            <h1 class="title">
+            CHANGE EMAIL
+            </h1>
+        </div>
+
+        <div class="container1">
+            <form>
+            <div class="row">
+                <div class="col-25">
+                <label for="currentpass">Current Password </label>
+                </div>
+                <div class="col-75">
+                <input type="text" id="currentpass" name="currentpass" placeholder="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                <label for="newpass">New Password </label>
+                </div>
+                <div class="col-75">
+                <input type="text" id="newpass" name="newpass" placeholder="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                <label for="retypepass">Re-type New Password </label>
+                </div>
+                <div class="col-75">
+                <input type="text" id="retypepass" name="retypepass" placeholder="">
+                </div>
+            </div>
+            <div class="row">
+                <button class="button is-rounded" id="save">Save Changes</button>
+            </div>
+            <div class="row">
+                <button class="button is-danger is-rounded" id="cancel">Cancel</button>
+            </div>
+            </form>
+        </div>
+
+    </div>
+    </section>
+
+    <div>
+        <p id="alrights">@2019 ZerterraPh</p>
+    </div>
+
+</body>
+
+</html>

7599a0a2e129cdef837eed0593e7c567ad74b564
slider44 <elizabeth.laguardia@outlook.ph>
2020-02-26 03:09:00 +0800

    bernard

--- zerterra_cms/searchFunction/searchApprovedFunction.php
+++ zerterra_cms/searchFunction/searchApprovedFunction.php
@@ -1,62 +1,58 @@
 <?php
 
 
-$sql= "SELECT * FROM approved_order_list WHERE CONCAT(`Firstname`, `Lastname`, `Email`, `Contact`, `Address`) LIKE '%".$searchValue."%'";
+$sql= "SELECT * FROM approved_order_list WHERE CONCAT(`SerialNumber`,`Firstname`, `Lastname`, `Email`, `Contact`) LIKE '%".$searchValue."%'";
  //$sql = "SELECT * FROM request_list WHERE is_approved='0'" ;
 $result = $con->query($sql);
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) 
   {
-    $id = $row['id'];
-    $id1 = $row['Pending_id'];
-    $fname = $row['Firstname'];
-    $lname = $row['Lastname'];
-    $email = $row['Email'];
-    $contact = $row['ContactNumber'];
-    $address = $row['Address'];
+                        $id = $row['id'];
+                        $orderNum = $row['OrderNumber'];
+                        $serialNum = $row['SerialNumber'];
+                        $fname = $row['Firstname'];
+                        $lname = $row['Lastname'];
+                        $email = $row['Email'];
+                        $contact = $row['Contact'];
+                        $address = $row['Address'];
     
     ?>
     
     <tr>
      
 
-      <td>
-       # <?php echo $id; ?>
-     </td>
-     
-     <td>
-      <?php echo $id1; ?>
-    </td>
+    <td>
+                             # <?php echo $id; ?>
+                         </td>
+                         <td>
+                            <?php echo $serialNum; ?>
+                        </td>
+                        <td>
+                            <?php echo $fname; ?>
+                        </td>
 
-    <td>
-      <?php echo $fname; ?>
-    </td>
+                        <td>
+                            <?php echo $lname; ?>
+                        </td>
 
-    <td>
-      <?php echo $lname; ?>
-    </td>
+                        <td>
+                            <?php echo $email; ?>
+                        </td>
 
-    <td>
-      <?php echo $email; ?>
-    </td>
-
-    <td>
-      <?php echo $contact; ?>
-    </td>
-    
-    <td>
-      <?php echo $address; ?>
-    </td>
-    
-    <td>
-      <button data-target="#delivered<?php echo $id;?>" class="button is-success is-small modal-button" id="btn_update" name="btn-update"><i class="far fa-edit"></i></i>
-       </button>
-          <?php
-             include 'Buttons/markAsDeliveredOrderModal.php';
-           ?> 
+                        <td>
+                            <?php echo $contact; ?>
+                        </td>
 
 
-         </td>
+                        <td>
+                            <button data-target="#delivered<?php echo $id;?>" class="button is-success is-small modal-button" id="btn_update" name="btn-update"><i class="far fa-edit"></i></i>
+                            </button>
+                            <?php
+                            include 'Buttons/markAsDeliveredOrderModal.php';
+                            ?> 
+
+
+                        </td>
 
 
 

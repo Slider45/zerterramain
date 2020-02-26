807b708f703617c965259b6315f64df0b65f2f4e
slider44 <elizabeth.laguardia@outlook.ph>
2020-02-26 09:06:58 +0800

    siomai

--- PagesFunction/query_request.php
+++ PagesFunction/query_request.php
@@ -4,8 +4,24 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 if (isset($_POST['saveRequest'])) { 
 
+	$sql = "SELECT id FROM request_repair_list ";
+	$result=mysqli_query($con,$sql);
+	$requestCount=mysqli_num_rows($result);
+	$requestCount++;
 
-	
+
+	$requestnum=date("ymd-Hi-") . 0 .$requestCount;
+	include '../content/requestMailtoUserContent.php';
+
+	$_SESSION['SN'] = $SNumber;
+	$_SESSION['FN'] = $fname;
+	$_SESSION['LN'] = $lname;
+	$_SESSION['EM'] = $email;
+	$_SESSION['CN'] = $contact;
+	$_SESSION['AD'] = $address;
+	$_SESSION['RN'] = $requestnum;
+
+
 	if(($Content = file_get_contents("../content/requestMailtoUserContent.php")) === false) {
 		$Content = "";
 	}
@@ -14,22 +30,16 @@ if (isset($_POST['saveRequest'])) {
 	$headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
 
-	$sql = "SELECT id FROM request_repair_list ";
-	$result=mysqli_query($con,$sql);
-	$requestCount=mysqli_num_rows($result);
-	$requestCount++;
 
-
-	$requestnum=date("ymd-Hi-") . 0 .$requestCount;
 
 
 	
 
-	$SNumber = $_POST['serialNum'];
+	$SNumber = $_POST['SNumber'];
 	$fname = $_POST['Fname'];
 	$lname = $_POST['Lname'];
 	$email = $_POST['email'];
-	$contact = $_POST['contactNum'];
+	$contact = $_POST['contact'];
 	$address = $_POST['address'];
 	
 	$email_to_support = "support@zerterra.com";
@@ -66,7 +76,7 @@ if (isset($_POST['saveRequest'])) {
 			if($con->query($cmdsql) === TRUE)
 			{
 				echo "<script>alert('Return Mail Sent!'); </script>";
-				echo '<script>window.location.href="../"</script>';
+				
 
 			}else{
 				echo "<script>alert('QUERY FAILED!'); </script>";
@@ -80,158 +90,5 @@ if (isset($_POST['saveRequest'])) {
 		echo "<script>alert('Sending Request Failed!'); </script>";
 
 
-	}
+	}echo '<script>window.location.href="../"</script>';
 }
-
-
-
-	
-	// $cmdsql= "INSERT INTO request_repair_list(RequestNumber, SerialNumber, Firstname, Lastname, Email, Contact, Address, is_approved) VALUES ('$requestnum','$serialNum','$fname','$lname','$email','$contact','$address','0')";
-
-	// if($con->query($cmdsql) === TRUE)
-	// {
-
-
-
-
-
-
-	// 	function clean_string($string)
-	// 	{
-	// 		$bad = array("content-type", "bcc:", "to:", "cc:", "href");
-	// 		return str_replace($bad, "", $string);
-	// 	}
-
-	// 	$email_message = "Request Repair details below.\n\n";
-	// 	$email_message .= "Email: " . clean_string($email) . "\n";
-	// 	$email_message .= "First Name: " . clean_string($fname) . "\n";
-	// 	$email_message .= "Last Name: " . clean_string($lname) . "\n";
-	// 	$email_message .= "Contact number: " . clean_string($contact) . "\n";
-	// 	$email_message .= "Address: " . clean_string($address) . "\n";
- //     // $email_message .= "Message: " . clean_string($message) . "\n";
-
- //    // create email headers
-	// 	$headers = 'From: ' . $email_to . "\r\n" .
-	// 	'Reply-To: ' . $email . "\r\n" .
- //    	// 'X-Mailer: PHP/' . phpversion();
-	// 	$mail = mail($email_to, $email_subject, $email_message, $headers);
-	// 	if( $mail === true ) {
-
-	// 		if(($Content = file_get_contents("content/requestMailtoUserContent.php")) === false) {
-	// 			$Content = "";
-	// 		}
-	// 		$headers[] = 'MIME-Version: 1.0';
-	// 		$headers[] = 'Content-type: text/html; charset=iso-8859-1';
-
-	// 		$subject = "REQUEST REPAIR ( Request #: [ " . $requestnum . " ])";
-
-
-	// 		$_SESSION["reqNumber"] = $requestnum; 
-
-	// 		include 'content/requestMailtoUserContent.php';
-
-	// 		use PHPMailer\PHPMailer\PHPMailer;
-
-	// 		include_once "PHPMailer/PHPMailer.php";
-
-	// 		$mail = new PHPMailer();
-	// 		$mail->setFrom('no-reply@zerterra.com');
-	// 		$mail->addAddress($email, $fname . " " . $lname);
-	// 		$mail->Subject = $subject ;
-	// 		$mail->isHTML(true);
-
-	// 		$mail->Body=$Content;
-	// 		$mail->Header = implode("\r\n", $headers);
-
-
-
-
-
-
-
-
-
-	// 	}else{
-
-
-	// 		echo "<script>alert('Message not sent! Please try again!'); </script>";
-
-
-	// 	}
-
-	// }}
-
-
-
-// 		$to = "support@zerterra.com";
-// 		function clean_string($string)
-// 		{
-// 			$bad = array("content-type", "bcc:", "to:", "cc:", "href");
-// 			return str_replace($bad, "", $string);
-// 		}
-
-// 		$body .= "Email: <" . clean_string($email) . ">\n";
-// 		$body .= "Name: " . clean_string($fname . " " .$lname) . "\n";
-// 		$body .= "Serial Number: " . clean_string($serialNum) . "\n";
-// 		$body .= "Request Number: " . clean_string($requestnum) . "\n";
-// 		$body .= "Address: " . clean_string($address) . "\n";
-
-// 		$header = array(
-// 			'From' => $email,
-// 			'Reply-To' => 'support@zerterra.com',
-// 			'X-Mailer' => 'PHP/' . phpversion()
-// 		);
-
-// 		$header = 'REQUEST REPAIR FROM USER';
-
-// 		if(!empty($fname) && !empty($lname) && !empty($email) && !empty($serialNum)){
-
-
-
-// 			$subject = "REQUEST REPAIR!";
-
-// 			$mail = mail ($to,$subject,$body,$header);
-// 			if( $mail === true ) {
-
-// 				echo "<script>alert('REQUEST SENT!');</script>";
-// 				include 'PagesFunction/returnMailtoUser.php';							
-
-
-
-// 			}else{
-
-// 				$resultmsg =  " Sorry there was an error sending your message. \n Please try again later " ;
-// 				echo "<script>window.alert(".$resultmsg;");</script>";
-// 			}
-
-
-// 		}else{
-
-
-
-// 			$resultmsg="INVALID DATA \n PLEASE CHECK YOU INFOMATION!";
-// 			echo "<script>window.alert(".$resultmsg;");</script>";
-
-
-
-
-// 		}
-
-
-
-// 	}else{
-// 		echo "<script>alert('Request Not Saved!');</script>";
-// 	}
-
-
-
-// }
-
-
-
-
-
-
-
-
-	?>
--- content/requestMailtoUserContent.php
+++ content/requestMailtoUserContent.php
@@ -1,81 +1,79 @@
 <?php
 session_start();
 
-include 'PagesFunction/connection.php';
-$requestNum = $_SESSION["reqNumber"]; 
+include '../PagesFunction/connection.php';
+// $requestNum = $_SESSION["reqNumber"]; 
+$SNumber = $_SESSION['SN'] ;
+$fname = $_SESSION['FN'];
+$lname  = $_SESSION['LN'];
+$email = $_SESSION['EM'];
+$contact = $_SESSION['CN'];
+$address = $_SESSION['AD'];
+$requestnum = $_SESSION['RN'];
 
-$sql ="SELECT * FROM request_repair_list WHERE RequestNumber = $requestNum";
-$res_data = $con->query($sql);
-while($row = mysqli_fetch_array($res_data)){
 
-    $serialNum = $row['SerialNumber'];
-    $fname = $row['Firstname'];
-    $lname = $row['Lastname'];
-    $contact = $row['Contact'];
-    $email = $row['Email'];
-    $address = $row['Address'];
 
-    ?>
+?>
 
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>PRE ORDER</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-    <body style="margin: 0; padding: 0;">
-        <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
-            <tr>
-                <td style="padding: 10px 0 30px 0;">
-                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
-                        <tr>
-                            <td align="center" bgcolor="#6fbf6d" style="padding: 30px 0 20px 0; color: white; font-size: 28px; font-weight: bold; font-family: Montserrat;">
-                                <img src="http://zerterra.com/images/plainlogoWhite.png"  style="display: block; width:100%; max-width:150px;" />
-                                <p>REQUEST REPAIR</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
-                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                                    <tr>
-                                        <td style="color: #333; font-family: Arial, sans-serif; font-size: 24px;">
-                                            <b>Hi <?php echo $fname;?>!<br></b>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="padding: 20px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>PRE ORDER</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="margin: 0; padding: 0;">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+        <tr>
+            <td style="padding: 10px 0 30px 0;">
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
+                    <tr>
+                        <td align="center" bgcolor="#6fbf6d" style="padding: 30px 0 20px 0; color: white; font-size: 28px; font-weight: bold; font-family: Montserrat;">
+                            <img src="http://zerterra.com/images/plainlogoWhite.png"  style="display: block; width:100%; max-width:150px;" />
+                            <p>REQUEST REPAIR</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                <tr>
+                                    <td style="color: #333; font-family: Arial, sans-serif; font-size: 24px;">
+                                        <b>Hi <?php echo $fname;?>!<br></b>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 20px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
 
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                                                <tr>
-                                                    <td width="260" valign="top">
-                                                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                                                            <tr>
-                                                                <td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td width="260" valign="top">
+                                                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                                        <tr>
+                                                            <td>
 
-                                                                    <p style=" line-height: 2; font-family: Montserrat;font-weight: bold; font-size:  11pt; text-align:justify;">
-                                                                        we already received your email for the request repair of your machine from our website (zerterra.com)
-                                                                        Please wait while we are processing your
-                                                                        request. Make sure that all of your informations are correct.
-                                                                    </p>
-                                                                    <p style="  font-family: Montserrat;font-weight: bold; font-size:  11pt; text-align:justify;"> Thank you</p>
-                                                                    <p style="  font-family: Montserrat;font-weight: bold; font-size:  11pt; text-align:justify;">God bless!</p>
-                                                                </td>
-                                                            </tr>
+                                                                <p style=" line-height: 2; font-family: Montserrat;font-weight: bold; font-size:  11pt; text-align:justify;">
+                                                                    we already received your email for the request repair of your machine from our website (zerterra.com)
+                                                                    Please wait while we are processing your
+                                                                    request. Make sure that all of your informations are correct.
+                                                                </p>
+                                                                <p style="  font-family: Montserrat;font-weight: bold; font-size:  11pt; text-align:justify;"> Thank you</p>
+                                                                <p style="  font-family: Montserrat;font-weight: bold; font-size:  11pt; text-align:justify;">God bless!</p>
+                                                            </td>
+                                                        </tr>
 
-                                                            <hr>
-                                                            <tr>
-                                                                <td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+                                                        <hr>
+                                                        <tr>
+                                                            <td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
 
-                                                                    <table style="width:100%; height:200px;">
-                                                                        <tr style="color: #000; font-family:Montserrat; font-size: 16px; line-height: 20px; font-weight:bold; margin-bottom:50px;">
+                                                                <table style="width:100%; height:200px;">
+                                                                    <tr style="color: #000; font-family:Montserrat; font-size: 16px; line-height: 20px; font-weight:bold; margin-bottom:50px;">
                                                                         <td>Serial #:</td>
-                                                                        <td style="padding-left:10px; margin-bottom:50px;"><?php echo $serialNum;?></td>    
+                                                                        <td style="padding-left:10px; margin-bottom:50px;"><?php echo $SNumber;?></td>    
                                                                     </tr>
-                                                                      <tr style="color: #000; font-family:Montserrat; font-size: 16px; line-height: 20px; font-weight:bold; margin-bottom:50px;">
+                                                                    <tr style="color: #000; font-family:Montserrat; font-size: 16px; line-height: 20px; font-weight:bold; margin-bottom:50px;">
                                                                         <td>Firstname:</td>
                                                                         <td style="padding-left:10px; margin-bottom:50px;"><?php echo $fname;?></td>    
                                                                     </tr>
@@ -113,7 +111,7 @@ while($row = mysqli_fetch_array($res_data)){
                                                         <tr>
                                                             <td style="padding: 150px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
                                                                <div style="color:#4a8169; background-color:#d1d1d1; text-align:center; height:50px; padding-top:30px; font-size:15pt; font-weight:bold;">
-                                                                   <?php echo $requestNum;}?>
+                                                                   <?php echo $requestnum;?>
                                                                </div>
                                                                <p style="text-align:center; font-family:Montserrat; font-size:10pt;  font-weight:bold;">REQUEST NUMBER</p>
                                                            </td>
